776f6c3e28d1f4abc65701dd5688f6ebe5bd003a
Simone Mosciatti <smosciatti@rippling.com>
2026-01-01 22:44:13 +0000

    [ACEM] RUFF first
    Policy: Ruff 0.14.8
    Fixed: 3 violations
    
    Original Violations:
    
    src/movies/models/__init__.py:
    F403 `from .imdb import *` used; unable to detect undefined names
     --> src/movies/models/__init__.py:1:1
      |
    1 | from .imdb import *
      | ^^^^^^^^^^^^^^^^^^^
    2 | from .motn import *
    3 | from .user import *
      |
    
    F403 `from .motn import *` used; unable to detect undefined names
     --> src/movies/models/__init__.py:2:1
      |
    1 | from .imdb import *
    2 | from .motn import *
      | ^^^^^^^^^^^^^^^^^^^
    3 | from .user import *
      |
    
    F403 `from .user import *` used; unable to detect undefined names
     --> src/movies/models/__init__.py:3:1
      |
    1 | from .imdb import *
    2 | from .motn import *
    3 | from .user import *
      | ^^^^^^^^^^^^^^^^^^^
      |
    
    Found 3 errors.
    
    Automated by ACEM - Initiative #1

--- src/movies/models/__init__.py
+++ src/movies/models/__init__.py
@@ -1,3 +1,14 @@
-from .imdb import *
-from .motn import *
-from .user import *
+from .imdb import ImdbGenre, ImdbMovie, ImdbMovieGenre, ImdbTitleType
+from .motn import MotnGenre, MotnShow, MotnShowGenre
+from .user import UserViewInteraction
+
+__all__ = [
+    "ImdbGenre",
+    "ImdbMovie",
+    "ImdbMovieGenre",
+    "ImdbTitleType",
+    "MotnGenre",
+    "MotnShow",
+    "MotnShowGenre",
+    "UserViewInteraction",
+]
